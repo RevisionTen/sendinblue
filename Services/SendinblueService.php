0afc2fb59ffd4b4b21dc5a6afed7d4c5785eac21
TAR5 <jessiepopel@icloud.com>
2021-08-30 11:27:07 +0200

    Added subscribeWithDoubleOptIn method and added doi_template_id config parameter

--- Services/SendinblueService.php
+++ Services/SendinblueService.php
@@ -10,6 +10,7 @@ use SendinBlue\Client\Api\ContactsApi;
 use SendinBlue\Client\ApiException;
 use SendinBlue\Client\Configuration;
 use SendinBlue\Client\Model\CreateContact;
+use SendinBlue\Client\Model\CreateDoiContact;
 
 class SendinblueService
 {
@@ -22,10 +23,8 @@ class SendinblueService
         $this->config = $config;
 
         $apiConfig = Configuration::getDefaultConfiguration()->setApiKey('api-key', $config['api_key']);
-        $this->apiInstance = new ContactsApi(
-            new Client(),
-            $apiConfig
-        );
+        $client = new Client();
+        $this->apiInstance = new ContactsApi($client, $apiConfig);
     }
 
     /**
@@ -47,12 +46,52 @@ class SendinblueService
         }
 
         $contact = new CreateContact();
-        $contact['email'] = $email;
-        $contact['attributes'] = $attributes;
-        $contact['listIds'] = [
-            $this->config['campaigns'][$campaign]['list_id'],
-        ];
+        $contact->setEmail($email);
+        $contact->setAttributes((object) $attributes);
+
+        $listId = (int) $this->config['campaigns'][$campaign]['list_id'] ?? 1;
+        $contact->setListIds([$listId]);
+
         $this->apiInstance->createContact($contact);
+
+        return true;
+    }
+
+    /**
+     * Subscribes a user to a list and require a double opt-in.
+     *
+     * @param string $campaign
+     * @param string $email
+     * @param string $source
+     * @param array  $attributes
+     * @param array  $redirectionUrl
+     *
+     * @return bool
+     *
+     * @throws Exception
+     */
+    public function subscribeWithDoubleOptIn(string $campaign, string $email, string $source = null, array $attributes = [], string $redirectionUrl): bool
+    {
+        if (!isset($this->config['campaigns'][$campaign])) {
+            return false;
+        }
+
+        $contact = new CreateDoiContact();
+        $contact->setEmail($email);
+        $contact->setAttributes((object) $attributes);
+
+        $listId = (int) $this->config['campaigns'][$campaign]['list_id'] ?? 1;
+        $contact->setIncludeListIds([$listId]);
+
+        $templateId = $this->config['campaigns'][$campaign]['doi_template_id'] ?? null;
+        if (null === $templateId) {
+            throw new Exception('You must provide a doi_template_id');
+        }
+        $contact->setTemplateId((int) $templateId);
+
+        $contact->setRedirectionUrl($redirectionUrl);
+
+        $this->apiInstance->createDoiContact($contact);
 
         return true;
     }
