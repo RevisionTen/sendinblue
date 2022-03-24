954b702d72b36f594ab13be3df75b145fab9ecaf
Cornelius Kahns <kahns@bdrops.de>
2022-03-24 16:26:05 +0100

    Fixed subscribing to multiple lists

--- SendinblueBundle.php
+++ SendinblueBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SendinblueBundle extends Bundle
 {
-    public const VERSION = '1.0.1';
+    public const VERSION = '1.0.2';
 
     public function boot(): void
     {
